ee17856ede562555eeaafe5ac56a4f05586ccc23
Harshal Sakhare <harshalsakhare2305@gmail.com>
2026-06-13 10:13:21 +0530

    JDBC connections

---
+++ JDBC/Main.java
@@ -0,0 +1,44 @@
+package JDBC;
+
+import java.sql.*;
+
+public class Main {
+
+   private static  final String url ="jdbc:mysql://localhost:3306/harshal1";
+ private static final String username="root";
+  private static final String password="********";
+
+    public static void main(String[] args) {
+
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try {
+            Connection con = DriverManager.getConnection(url,username,password);
+            Statement state =con.createStatement();
+
+            String query = "select * from employees";
+            ResultSet result = state.executeQuery(query);
+
+            System.out.println("  "+"ID"+ "  "+"Name"+"  "+"Salary "+"  ");
+
+            while(result.next()){
+                int id = result.getInt(1);
+                String name =result.getString(2);
+                int salary =result.getInt(3);
+                System.out.println("  "+id+ "  "+name+"  "+salary+"  ");
+            }
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+}
